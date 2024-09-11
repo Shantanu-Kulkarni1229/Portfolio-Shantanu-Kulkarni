fdfc64d507717db43cf84435602c85f8152f2e51
shantanumusic <shantanukulkarnimusic@gmail.com>
2024-09-11 23:56:37 +0530

    Your commit message here

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#007BFF',
+      },
+    },
+  },
+};
